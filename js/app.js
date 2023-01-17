d4a8b12e476f8b4787d64023e2c27a0779a15231
Jamie Cosham <jamie.cosham@netmatters-scs.com>
2023-01-17 22:44:22 +0000

    Page Transition
    Added Animsition to control transition effects between pages.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+// ========================================
+// Main JavaScript
+// ========================================
+
+// Deactivate All Fallback Elements
+
 $('.no-script').hide();
+
+// Activate Script Elements
+
 $('.script').show();
+
+// Off Screen Menu
 
 const hamMenu = document.querySelector('.hamburger-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
@@ -9,7 +20,18 @@ hamMenu.addEventListener('click', () => {
 	offScreenMenu.classList.toggle('active');
 });
 
+// Hide Menu On Link Click
+
 $("logo-mobile-container a, nav a").on("click", function() {
     $(".hamburger-menu").removeClass("active");
     $(".off-screen-menu").removeClass("active");
+});
+
+// Page Transition
+
+$('.animsition').animsition({
+	inClass: 'flip-in-y',
+	outClass: 'fade-out-up-lg',
+	inDuration: 1250,
+	outDuration: 750
 });
