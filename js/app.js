6057ac5b3502144a565b754f08a319c37dcad42d
Jamie Cosham <jamie.cosham@netmatters-scs.com>
2023-01-17 16:39:46 +0000

    Updates
    Cleaned code and removed repeating styles. Auto hide off screen menu. Updated skills list. Optimised and updated headings.

--- js/app.js
+++ js/app.js
@@ -8,3 +8,8 @@ hamMenu.addEventListener('click', () => {
 	hamMenu.classList.toggle('active');
 	offScreenMenu.classList.toggle('active');
 });
+
+$("logo-mobile-container a, nav a").on("click", function() {
+    $(".hamburger-menu").removeClass("active");
+    $(".off-screen-menu").removeClass("active");
+});
