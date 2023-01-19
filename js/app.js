266398bda45e67d7745b63b93b360b620bc2e59a
Jamie Cosham <jamie.cosham@netmatters-scs.com>
2023-01-19 08:53:00 +0000

    Form Validation Stage 1
    Added JavaScript validation with feedback to user, error message, success / failure icons and styling. No Regex validation yet, currently just checking for empty strings and trimming input.

--- js/app.js
+++ js/app.js
@@ -10,10 +10,45 @@ $('.no-script').hide();
 
 $('.script').show();
 
+// Form Validation
+
+let form = document.getElementById("form"),
+	firstName = document.getElementById("name"),
+	surname	= document.getElementById("surname"),
+	email = document.getElementById("email"),
+	subject	= document.getElementById("subject"),
+	message	= document.getElementById("message"),
+	errorMsg = document.getElementsByClassName("error"),
+	successIcon = document.getElementsByClassName("success-icon"),
+	failureIcon = document.getElementsByClassName("failure-icon");
+	
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	hasContent(firstName, 0, "First Name cannot be blank");
+	hasContent(surname, 1, "Last Name cannot be blank");
+	hasContent(email, 2, "Email cannot be blank");
+	hasContent(subject, 3, "Subject cannot be blank");
+	hasContent(message, 4, "Message cannot be blank");
+});
+
+let hasContent = (id, index, error) => {
+	if (id.value.trim() === "")	{
+		errorMsg[index].innerHTML = error;
+	    id.style.border = "2px solid red";
+	    failureIcon[index].style.opacity = "1";
+	    successIcon[index].style.opacity = "0";
+	    return;
+	}
+	errorMsg[index].innerHTML = "";
+    id.style.border = "2px solid green";
+    failureIcon[index].style.opacity = "0";
+    successIcon[index].style.opacity = "1";
+};
+
 // Off Screen Menu
 
-const hamMenu = document.querySelector('.hamburger-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
+const	hamMenu			= document.querySelector('.hamburger-menu'),
+		offScreenMenu	= document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
 	hamMenu.classList.toggle('active');
@@ -22,7 +57,7 @@ hamMenu.addEventListener('click', () => {
 
 // Hide Menu On Link Click
 
-$("logo-mobile-container a, nav a").on("click", function() {
+$(".logo-mobile-container a, nav a").on("click", function() {
     $(".hamburger-menu").removeClass("active");
     $(".off-screen-menu").removeClass("active");
 });
@@ -32,6 +67,14 @@ $("logo-mobile-container a, nav a").on("click", function() {
 $('.animsition').animsition({
 	inClass: 'flip-in-y',
 	outClass: 'fade-out-up-lg',
-	inDuration: 1250,
+	inDuration: 1000,
 	outDuration: 750
+});
+
+// Typewriter Effect
+
+$(function() {
+	$('.name').typewriter({
+		sound:true
+	});
 });
